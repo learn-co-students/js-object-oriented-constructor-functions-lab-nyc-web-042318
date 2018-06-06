adc1a26d4d0cfab62f6456c58dcabe0553602e1b
Mendy Wolosow <mendywoly@gmail.com>
2018-06-05 22:47:32 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function Scooter (year, color, model) {
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+function Driver (year, color, model) {
+  this.name = year
+  this.age = color
+  this.experience = model
+}
+
+
+function PickupLocation (year, color) {
+  this.address = year
+  this.city = color
+}
